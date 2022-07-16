ce8aacd66c24d1f88f0fb8e48b73bc8ded35b253
Bohdan <brusnachenko@gmail.com>
2022-07-16 22:10:28 +0300

    make it better

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(express.json({extended: true}));
 
 app.use('/api', router);
-
+__dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
