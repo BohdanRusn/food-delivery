fb4f643aa3552e66bbbfe1506597bac6c4da831a
Bohdan <brusnachenko@gmail.com>
2022-07-16 20:23:01 +0300

    make it better1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORT = process.env.PORT || config.get('port') || 3033;
+const PORT = process.env.PORT || config.get('port');
 
 async function start() {
   try {
