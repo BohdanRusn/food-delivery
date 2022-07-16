4a781bab66473a3a28e7eacdbd1ff4e8ecf2b83d
Bohdan <brusnachenko@gmail.com>
2022-07-16 21:37:58 +0300

    prep for deploy

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use(express.json({extended: true}));
 app.use('/api', router);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
-const PORT = config.get('port');
+const PORT = config.get('port') || 3033;
 
 async function start() {
   try {
