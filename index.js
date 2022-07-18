b4929bcea57eab70e7f68f4b2dfc2d71b31aa0d5
Bohdan <brusnachenko@gmail.com>
2022-07-18 10:47:40 +0300

    deploy prep 250

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.json({extended: true}));
 app.use('/api', router);
 __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname));
+  app.use(express.static('/', path.join(__dirname, '/client', '/build')));
 
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 } else {
